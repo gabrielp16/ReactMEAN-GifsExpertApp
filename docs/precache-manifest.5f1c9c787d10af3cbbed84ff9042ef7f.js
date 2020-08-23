db813fe8dac2f2d4044c9f81c9e05c538bbdfe7c
Gabriel Peña <gabriel.p16@gmail.com>
2020-08-22 23:15:36 -0500

    Cambio de carpeta build por docs

---
+++ docs/precache-manifest.5f1c9c787d10af3cbbed84ff9042ef7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613f876f7ed02ba7fc1d0585047b0dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95a2ca7bdc9d762fac75",
+    "url": "/static/css/main.30752fd6.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a2ca7bdc9d762fac75",
+    "url": "/static/js/main.aae4856e.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
